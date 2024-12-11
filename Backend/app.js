3b7076180512ca9ca496075d7a2fea6779b47255
Achint Jain <achintjain2004@gmail.com>
2024-12-11 14:16:36 +0530

    user auth done

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const connectToDb = require('./db/db');
@@ -13,7 +14,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 
 
